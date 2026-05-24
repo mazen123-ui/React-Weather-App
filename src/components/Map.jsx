e436d331821306aef0ed47bf4663d1367aef2521
Mazen Medhat <mazenmedhat458@gmail.com>
2026-05-25 00:17:03 +0300

    fix map marker 2

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,31 +2,36 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x,
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
+const DefaultIcon = L.icon({
+  iconUrl: "/React-Weather-App/marker-icon.png",
+  iconRetinaUrl: "/React-Weather-App/marker-icon-2x.png",
+  shadowUrl: "/React-Weather-App/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
 });
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 import { useWeather } from "../Context/WeatherContext";
 
 export default function Map() {
   const { lat, long, city } = useWeather();
+
   return (
     <MapContainer
       center={[lat, long]}
       zoom={10}
-      style={{ height: "400px", width: "400px", borderRadius: "15px", }}
+      style={{
+        height: "400px",
+        width: "400px",
+        borderRadius: "15px",
+      }}
     >
       <TileLayer
         url="https://api.maptiler.com/maps/hybrid-v4/{z}/{x}/{y}.jpg?key=6xn4qJh0o1Hjrpwia5b6"
-        attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+        attribution="&copy; OpenStreetMap contributors"
       />
+
       <Marker position={[lat, long]}>
         <Popup>
           <b>{city}</b>
