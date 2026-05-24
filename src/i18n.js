e7f89aa972079d555b8560643eedfcacbc5f99fe
Mazen Medhat <mazenmedhat458@gmail.com>
2026-05-25 02:04:56 +0300

    fix paths

--- src/i18n.js
+++ src/i18n.js
@@ -14,7 +14,7 @@ i18n
       caches: ["cookie"],
     },
     backend: {
-      loadPath: `${import.meta.env.BASE_URL}locales/{{lng}}/translations.json`,
+      loadPath: `./locales/{{lng}}/translations.json`,
     },
     interpolation: {
       escapeValue: false,
